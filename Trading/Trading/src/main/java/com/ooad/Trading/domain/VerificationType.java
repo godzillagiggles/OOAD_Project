e7bb4427a911ebe1bf7c97ffe732e00f9f501738
Sriganesh G <128352300+godzillagiggles@users.noreply.github.com>
2025-04-02 17:53:15 +0530

    Delete Trading/Trading directory

--- Trading/Trading/src/main/java/com/ooad/Trading/domain/VerificationType.java
+++
@@ -1,7 +0,0 @@
-package com.ooad.Trading.domain;
-
-public enum VerificationType {
-    MOBILE,
-    EMAIL
-
-}
--- Trading/Trading/src/main/java/com/ooad/Trading/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ooad.Trading.repository;
-
-import com.ooad.Trading.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Member, Long> {
-
-
-}
